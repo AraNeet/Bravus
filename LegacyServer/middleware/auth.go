f419a32013fddb416705af3c04ee97732c4e26d9
Aramis Martinez <135170950+AramisAra@users.noreply.github.com>
2024-11-22 16:35:49 -0400

    Feat: Auth file hasn't been refactored. Just delete the JWT create code to refactor for more security. TODO: Read up and JWT for better generation of the tokens

--- LegacyServer/middleware/auth.go
+++ LegacyServer/middleware/auth.go
@@ -1,18 +1,10 @@
 package middlewares
 
 import (
-	"github.com/AramisAra/BravusBackend/config"
 	"github.com/gofiber/fiber/v2"
-	jwtware "github.com/gofiber/jwt/v3"
 )
 
-func NewAuthMiddleware() fiber.Handler {
-	return jwtware.New(jwtware.Config{
-		SigningKey:   []byte(config.Secret),
-		ContextKey:   "user",
-		ErrorHandler: jwtError,
-	})
-}
+func NewAuthMiddleware() {}
 
 func jwtError(c *fiber.Ctx, err error) error {
 	if err.Error() == "Missing or malformed JWT" {
