d2747862435bbce569d7cfc3da2394a9daca9724
Aramis Martinez <135170950+AramisAra@users.noreply.github.com>
2024-11-22 16:31:37 -0400

    Feat: Moved Auth and Function to the root dir to hold for refactoring in the future. TODO: Recreate the serialization function for use with the new models. TODO: Last thing refactoring of the Auth function for google sheet api

--- auth.go
+++ auth.go
@@ -1,4 +1,4 @@
-package googleapis
+package Bravus
 
 import (
 	"context"
--- function.go
+++ function.go
@@ -1,9 +1,9 @@
-package dbmodels
+package Bravus
 
-// Remove the import cycle between utilsmodels and dbmodels
+import "github.com/AramisAra/BravusBackend/database/models"
 
-func CreateJoinResultClient(client Client, animal Animal) joinResultClient {
-	return joinResultClient{Client: client, Animal: animal}
+func CreateJoinResultClient(client Client, animal models.Animal) joinResultClient {
+	return joinResultClient{Client: client, models.Animal: animal}
 }
 
 func CreateClientResponse(client Client) ClientSerializer {
@@ -11,8 +11,8 @@ func CreateClientResponse(client Client) ClientSerializer {
 		Animals: client.Animals, Appointments: client.Appointments}
 }
 
-func CreateServiceResponse(service Service) ServiceSerializer {
-	return ServiceSerializer{ID: service.ID, ServiceName: service.NameService, ServiceDesc: service.ServiceDesc, Price: service.Price}
+func CreateServiceResponse(service models.Service) ServiceSerializer {
+	return ServiceSerializer{ID: service.ID, ServiceName: service.ServiceName, ServiceDesc: service.ServiceDesc, Price: service.Price}
 }
 
 func CreateOwnerResponse(owner Owner) OwnerSerializer {
@@ -20,11 +20,11 @@ func CreateOwnerResponse(owner Owner) OwnerSerializer {
 		Career: owner.Career, Services: owner.Services, Appointments: owner.Appointments}
 }
 
-func CreateAnimalResponse(animal Animal) AnimalSerializer {
+func CreateAnimalResponse(animal models.Animal) AnimalSerializer {
 	return AnimalSerializer{ID: animal.ID, Animal_Name: animal.Animal_Name, Animal_Specie: animal.Animal_Specie, Animal_Age: animal.Animal_Age, Client: animal.Client_id}
 }
 
-func CreateAppointmentResponse(appointment Appointment) AppointmentSerializer {
+func CreateAppointmentResponse(appointment models.Appointment) AppointmentSerializer {
 	return AppointmentSerializer{ClientID: appointment.ClientID, OwnerID: appointment.OwnerID, ServiceID: appointment.ServiceID,
 		Date: appointment.Date, Time: appointment.Time}
 }
